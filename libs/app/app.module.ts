689836d4570586a42b2a3e616cc74b4f68b6b1c1
choewy <choewy32@gmail.com>
2024-03-20 17:08:12 +0900

    feature/app module

--- libs/app/app.module.ts
+++ libs/app/app.module.ts
@@ -1,4 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
-@Module({})
-export class AppModule {}
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HttpLogMiddleware } from '../middlewares';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLogMiddleware).forRoutes('*');
+  }
+}
